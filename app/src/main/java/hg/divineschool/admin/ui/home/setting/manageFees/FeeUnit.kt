c73aafa8448e69cf85e67d3b42f04a427cb604de
hritikpgupta <hritikgupta722@gmail.com>
2023-03-24 16:59:25 +0000

    Created fee unit composable

--- app/src/main/java/hg/divineschool/admin/ui/home/setting/manageFees/FeeUnit.kt
+++ app/src/main/java/hg/divineschool/admin/ui/home/setting/manageFees/FeeUnit.kt
@@ -1,7 +1,6 @@
 package hg.divineschool.admin.ui.home.setting.manageFees
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,8 +10,35 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hg.divineschool.admin.ui.home.dashboard.registerStudent.FormEditText
 import hg.divineschool.admin.ui.theme.mediumFont
+
+
+@Composable
+fun FeeUnit(
+    modifier: Modifier,
+    editTextModifier: Modifier,
+    name: TextFieldValue,
+    textLable : String,
+    onValueChanged: (value: TextFieldValue) -> Unit
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = textLable, style = TextStyle(
+                fontFamily = mediumFont,
+                fontSize = 26.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            ), modifier = Modifier.padding(start = 8.dp, top = 4.dp)
+        )
+        FormEditText(textValue = name,
+            text = "Enter Tuition Fee",
+            keyboardType = KeyboardType.Text,
+            color = Color.Black,
+            modifier = editTextModifier,
+            isEnabled = true,
+            onValueChanged = { onValueChanged(it) })
+    }
+}
--- app/src/main/java/hg/divineschool/admin/ui/home/setting/manageFees/ManageFee.kt
+++ app/src/main/java/hg/divineschool/admin/ui/home/setting/manageFees/ManageFee.kt
@@ -1,9 +1,12 @@
 package hg.divineschool.admin.ui.home.setting.manageFees
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
@@ -13,14 +16,12 @@ import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.accompanist.insets.ui.Scaffold
 import hg.divineschool.admin.ui.home.DPSBar
-import hg.divineschool.admin.ui.home.dashboard.registerStudent.FormEditText
 import hg.divineschool.admin.ui.theme.mediumFont
 import kotlinx.coroutines.launch
 
@@ -28,6 +29,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun ManageFee(navController: NavController) {
     val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
     val bringIntoViewRequester = BringIntoViewRequester()
     val editTextModifier = Modifier
         .padding(horizontal = 10.dp)
@@ -53,8 +55,10 @@ fun ManageFee(navController: NavController) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(scrollState)
                 .padding(4.dp)
                 .padding(padding)
+                .background(color = Color.LightGray.copy(0.2f))
         ) {
             Text(
                 text = "Tuition Fee", style = TextStyle(
@@ -72,74 +76,34 @@ fun ManageFee(navController: NavController) {
                     .padding(8.dp)
             ) {
                 Row(modifier = Modifier.fillMaxWidth()) {
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(
-                            text = "Play Group", style = TextStyle(
-                                fontFamily = mediumFont,
-                                fontSize = 26.sp,
-                                color = Color.Black,
-                                fontWeight = FontWeight.Bold
-                            ), modifier = Modifier.padding(start = 8.dp, top = 4.dp)
-                        )
-                        FormEditText(textValue = playGroup,
-                            text = "Enter Tuition Fee",
-                            keyboardType = KeyboardType.Text,
-                            color = Color.Black,
-                            modifier = editTextModifier.padding(top = 8.dp),
-                            isEnabled = true,
-                            onValueChanged = { playGroup = it })
-                    }
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(
-                            text = "Lower Nursery", style = TextStyle(
-                                fontFamily = mediumFont,
-                                fontSize = 26.sp,
-                                color = Color.Black,
-                                fontWeight = FontWeight.Bold
-                            ), modifier = Modifier.padding(start = 8.dp, top = 4.dp)
-                        )
-                        FormEditText(textValue = ln,
-                            text = "Enter Tuition Fee",
-                            keyboardType = KeyboardType.Text,
-                            color = Color.Black,
-                            modifier = editTextModifier.padding(top = 8.dp),
-                            isEnabled = true,
-                            onValueChanged = { ln = it })
-                    }
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(
-                            text = "Upper Nursery", style = TextStyle(
-                                fontFamily = mediumFont,
-                                fontSize = 26.sp,
-                                color = Color.Black,
-                                fontWeight = FontWeight.Bold
-                            ), modifier = Modifier.padding(start = 8.dp, top = 4.dp)
-                        )
-                        FormEditText(textValue = un,
-                            text = "Enter Tuition Fee",
-                            keyboardType = KeyboardType.Text,
-                            color = Color.Black,
-                            modifier = editTextModifier.padding(top = 8.dp),
-                            isEnabled = true,
-                            onValueChanged = { un = it })
-                    }
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(
-                            text = "Class One", style = TextStyle(
-                                fontFamily = mediumFont,
-                                fontSize = 26.sp,
-                                color = Color.Black,
-                                fontWeight = FontWeight.Bold
-                            ), modifier = Modifier.padding(start = 8.dp, top = 4.dp)
-                        )
-                        FormEditText(textValue = classOne,
-                            text = "Enter Tuition Fee",
-                            keyboardType = KeyboardType.Text,
-                            color = Color.Black,
-                            modifier = editTextModifier.padding(top = 8.dp),
-                            isEnabled = true,
-                            onValueChanged = { classOne = it })
-                    }
+                    FeeUnit(
+                        modifier = Modifier.weight(1f),
+                        editTextModifier = editTextModifier.padding(start = 8.dp, top = 12.dp),
+                        name = playGroup,
+                        textLable = "Play Group",
+                        onValueChanged = { playGroup = it }
+                    )
+                    FeeUnit(
+                        modifier = Modifier.weight(1f),
+                        editTextModifier = editTextModifier.padding(start = 8.dp, top = 12.dp),
+                        name = ln,
+                        textLable = "Lower Nursery",
+                        onValueChanged = { ln = it }
+                    )
+                    FeeUnit(
+                        modifier = Modifier.weight(1f),
+                        editTextModifier = editTextModifier.padding(start = 8.dp, top = 12.dp),
+                        name = un,
+                        textLable = "Upper Nursery",
+                        onValueChanged = { un = it }
+                    )
+                    FeeUnit(
+                        modifier = Modifier.weight(1f),
+                        editTextModifier = editTextModifier.padding(start = 8.dp, top = 12.dp),
+                        name = classOne,
+                        textLable = "Class One",
+                        onValueChanged = { classOne = it }
+                    )
                 }
             }
         }
