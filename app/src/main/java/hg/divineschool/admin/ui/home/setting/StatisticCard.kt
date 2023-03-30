80fedffe0cb5813ffdb11617d223f9e5f4b868c3
hritikpgupta <hritikgupta722@gmail.com>
2023-03-30 10:42:25 +0100

    Working of statistic card

--- app/src/main/java/hg/divineschool/admin/ui/home/setting/StatisticCard.kt
+++ app/src/main/java/hg/divineschool/admin/ui/home/setting/StatisticCard.kt
@@ -1,24 +1,71 @@
 package hg.divineschool.admin.ui.home.setting
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 @Preview
 @Composable
 fun StatisticCard() {
-    Card( backgroundColor = Color.LightGray.copy(0.7f),
+    Card(
+        backgroundColor = Color.LightGray.copy(0.2f),
         modifier = Modifier
             .fillMaxWidth()
-            .requiredHeight(280.dp).padding(6.dp)
+            .requiredHeight(280.dp)
+            .padding(6.dp)
     ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(0.8f)
+                    .background(color = Color.Red.copy(0.3f))
+            ) {
 
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(end = 4.dp)
+                    .weight(0.2f)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = hg.divineschool.admin.R.drawable.open),
+                        contentDescription = "Open",
+                        modifier = Modifier.requiredSize(40.dp)
+                    )
+                    Spacer(modifier = Modifier.requiredWidth(4.dp))
+                    Text(text = "7:00 am")
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = hg.divineschool.admin.R.drawable.close),
+                        contentDescription = "Close",
+                        modifier = Modifier.requiredSize(40.dp)
+                    )
+                    Spacer(modifier = Modifier.requiredWidth(4.dp))
+                    Text(text = "3:00 pm")
+                }
+
+            }
+        }
 
     }
 }
