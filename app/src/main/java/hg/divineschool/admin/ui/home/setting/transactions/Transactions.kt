41be6e08fbc21573787d48f125b9930ae7e7f089
hritikpgupta <hritikgupta722@gmail.com>
2023-04-07 14:43:35 +0100

    Added date range picker

--- app/src/main/java/hg/divineschool/admin/ui/home/setting/transactions/Transactions.kt
+++ app/src/main/java/hg/divineschool/admin/ui/home/setting/transactions/Transactions.kt
@@ -1,20 +1,37 @@
 package hg.divineschool.admin.ui.home.setting.transactions
 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.accompanist.insets.ui.Scaffold
 import hg.divineschool.admin.ui.home.DPSBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Transactions(navController: NavController) {
-    Scaffold(scaffoldState = rememberScaffoldState(),
-        topBar = {
-            DPSBar(onBackPressed = {
-                navController.popBackStack()
-            }, className = "Transactions")
-        }
-    ) {
+    val dateRangeState = rememberDateRangePickerState()
+
+
+    Scaffold(scaffoldState = rememberScaffoldState(), topBar = {
+        DPSBar(onBackPressed = {
+            navController.popBackStack()
+        }, className = "Transactions")
+    }) {
+        DateRangePicker(state = dateRangeState,
+            modifier = Modifier
+                .fillMaxHeight()
+                .requiredWidth(500.dp)
+                .padding(top = 60.dp, start = 0.dp, bottom = 0.dp))
 
     }
 }
--- app/src/main/java/hg/divineschool/admin/ui/home/setting/transactions/TransactionsViewModel.kt
+++ app/src/main/java/hg/divineschool/admin/ui/home/setting/transactions/TransactionsViewModel.kt
@@ -1,11 +1,12 @@
 package hg.divineschool.admin.ui.home.setting.transactions
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hg.divineschool.admin.data.dashboard.settings.SettingRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class TransactionsViewModel @Inject constructor(
-    private var respository : SettingRepository
-) {
+    private var respository: SettingRepository
+) : ViewModel() {
 }
